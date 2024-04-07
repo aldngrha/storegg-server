75420ec18bfe13787792785a81a75f486e977130
Aldi Nugraha <aldi.nugrahatk@gmail.com>
2024-04-07 22:37:53 +0700

    Finishing admin dashboard cms

--- app/dashboard/controller.js
+++ app/dashboard/controller.js
@@ -1,23 +1,23 @@
-// const Transaction = require('../transaction/model')
-// const Voucher = require('../voucher/model')
-// const Player = require('../player/model')
+const Transaction = require('../transaction/model')
+const Voucher = require('../voucher/model')
+const Player = require('../player/model')
 const Category = require('../category/model')
 
 module.exports = {
   index: async (req, res) => {
     try {
 
-      // const transaction = await Transaction.countDocuments()
-      // const voucher = await Voucher.countDocuments()
-      // const player = await Player.countDocuments()
+      const transaction = await Transaction.countDocuments()
+      const voucher = await Voucher.countDocuments()
+      const player = await Player.countDocuments()
       const category = await Category.countDocuments()
       res.render('admin/dashboard/view_dashboard', {
         name: req.session.user.name,
         title: 'StoreGG - Dashboard',
         count: {
-          // transaction,
-          // player,
-          // voucher,
+          transaction,
+          player,
+          voucher,
           category
         }
       })
